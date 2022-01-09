0eea5b7c5c4c9c4b768131d6210bf07a5e118902
Ariel Raudsepp <ariel@Disthene.localdomain>
2022-01-08 18:25:48 -0800

    create DisplaySkillsGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import logo from './rustacean.svg';
 import './App.css';
 import { Skill, getSkills, DiaryEntry, submitDiaryEntry } from './FetchAPI';
-import { DisplaySkill } from './components/DisplaySkill';
+import { SkillsGroup } from './components/DisplaySkill';
 
 type CheckedSkills = {
     [key: number]: boolean;
 }
 
-type CatSkills = {
+type CategorizedSkills = {
     [key: string]: Skill[];
 }
 
@@ -21,11 +21,11 @@ function App() {
     getSkills('/skills').then(setSkills);
   }, []);
 
-  let cat_skills = skills.reduce((init: CatSkills, skill: Skill) => {
-      if (!init[skill.category]) {
-     init[skill.category] = [];
-   }
-   init[skill.category].push(skill);
+  let categorized_skills = skills.reduce((init: CategorizedSkills, skill: Skill) => {
+    if (!init[skill.category]) {
+      init[skill.category] = [];
+    }
+    init[skill.category].push(skill);
     return init
   }, {});
 
@@ -35,23 +35,15 @@ function App() {
 
   let checkSkills = Object.entries(checked).reduce((init: number[], [key, value]) => {
     if (value === true) {
-        init.push(parseInt(key))
+      init.push(parseInt(key))
     };
     return init
   }, []);
 
-function displayedSkills(cat: string) {
-    if (!cat_skills[cat]) {
-        return
-    };
-   return cat_skills[cat].map((skill: Skill) => {
-      return <DisplaySkill skill={skill} onChecked={updateChecked} checked={checked[skill.id]} key={skill.id} />
-    })
-  };
 
   const diaryentry: DiaryEntry = {
-      entry_date: new Date(),
-      skill_ids: checkSkills
+    entry_date: new Date(),
+    skill_ids: checkSkills
   };
 
   let handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -61,22 +53,10 @@ function displayedSkills(cat: string) {
 
   return (
     <div className="App">
-      <h2 className="ui header"><img src={logo} className="ui circular image" />DBT Skills </h2>
-      <div className="ui section divider"></div>
-      <div>
-        <h2>Mindfulness</h2>
-        {displayedSkills('mindfulness')}
-      </div>
-      <div className="ui section divider"></div>
-      <div>
-        <h2>Emotion Regulation</h2>
-        {displayedSkills('emotion_regulation')}
-      </div>
-      <div className="ui section divider"></div>
-      <div>
-        <h2>Distress Tolerance</h2>
-        {displayedSkills('distress_tolerance')}
-      </div>
+      <h1 className="ui header"><img src={logo} className="ui circular image" />DBT Skills </h1>
+      <SkillsGroup category={'mindfulness'} categorized_skills={categorized_skills} update_checked={updateChecked} checkedSkills={checked} />
+      <SkillsGroup category={'emotion_regulation'} categorized_skills={categorized_skills} update_checked={updateChecked} checkedSkills={checked} />
+      <SkillsGroup category={'distress_tolerance'} categorized_skills={categorized_skills} update_checked={updateChecked} checkedSkills={checked} />
       <button className="ui primary button" type="submit" onClick={handleClick}>Submit</button>
     </div >
   );
--- src/components/DisplaySkill.tsx
+++ src/components/DisplaySkill.tsx
@@ -1,14 +1,25 @@
 import { ChangeEvent } from 'react';
 import { Skill } from '../FetchAPI';
 
-interface DisplaySkillProps {
+interface ToggleSkillProps {
     skill: Skill,
     onChecked: (id: number) => void,
     checked: boolean
 }
 
 
-export function DisplaySkill(props: DisplaySkillProps) {
+interface SkillsGroupProps {
+    categorized_skills: {
+        [key: string]: Skill[];
+    },
+    category: string,
+    update_checked: (id: number) => void,
+    checkedSkills: {
+        [key: number]: boolean
+    }
+}
+
+export function ToggleSkill(props: ToggleSkillProps) {
     let { skill, onChecked, checked } = props;
     let onChange = (_event: ChangeEvent<HTMLInputElement>) => {
         onChecked(skill.id);
@@ -27,5 +38,33 @@ export function DisplaySkill(props: DisplaySkillProps) {
         </div >
     );
 }
+
+
+export function SkillsGroup(props: SkillsGroupProps) {
+    let { category, categorized_skills, update_checked, checkedSkills } = props;
+    let skills_group;
+    if (!categorized_skills[category]) {
+        skills_group = (
+            <div className="ui placeholder">
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>)
+    } else {
+        skills_group = (
+            categorized_skills[category].map((skill: Skill) => {
+                return <ToggleSkill skill={skill} onChecked={update_checked} checked={checkedSkills[skill.id]} key={skill.id} />
+            }))
+    }
+    return (
+        <div>
+            <div className="ui section divider"></div>
+            <div>
+                <h2>{category}</h2>
+                {skills_group}
+            </div>
+        </div>
+    );
+};
+
 
 
