f9b20ca6ff40c1ba029d65a10e7b57558af487f8
Ariel Raudsepp <ariel@Disthene.localdomain>
2022-01-09 18:20:18 -0800

    add skill group toggle and string interpolation

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,116 @@
-import React, { useEffect, useState } from 'react';
-import logo from './rustacean.svg';
-import './App.css';
-import { Skill, getSkills, DiaryEntry, submitDiaryEntry } from './FetchAPI';
-import { SkillsGroup } from './components/DisplaySkill';
+import React, { useEffect, useState, MouseEvent } from "react";
+import logo from "./rustacean.svg";
+import "./App.css";
+import { Skill, getSkills, DiaryEntry, submitDiaryEntry } from "./FetchAPI";
+import { SkillsGroup } from "./components/DisplaySkill";
+import { Accordion, AccordionTitleProps } from "semantic-ui-react";
 
 type CheckedSkills = {
-    [key: number]: boolean;
-}
+  [key: number]: boolean;
+};
 
 type CategorizedSkills = {
-    [key: string]: Skill[];
-}
+  [key: string]: Skill[];
+};
 
 function App() {
-
   const [skills, setSkills] = useState<Skill[]>([]);
   const [checked, setChecked] = useState<CheckedSkills>({});
+  const [activeIndex, setActiveIndex] = useState<number>(0);
 
   useEffect(() => {
-    getSkills('/skills').then(setSkills);
+    getSkills("/skills").then(setSkills);
   }, []);
 
-  let categorized_skills = skills.reduce((init: CategorizedSkills, skill: Skill) => {
-    if (!init[skill.category]) {
-      init[skill.category] = [];
-    }
-    init[skill.category].push(skill);
-    return init
-  }, {});
+  let categorized_skills = skills.reduce(
+    (init: CategorizedSkills, skill: Skill) => {
+      if (!init[skill.category]) {
+        init[skill.category] = [];
+      }
+      init[skill.category].push(skill);
+      return init;
+    },
+    {}
+  );
 
   let updateChecked = (id: number) => {
     setChecked({ ...checked, [id]: !checked[id] });
-  }
+  };
 
-  let checkSkills = Object.entries(checked).reduce((init: number[], [key, value]) => {
-    if (value === true) {
-      init.push(parseInt(key))
-    };
-    return init
-  }, []);
+  let checkSkills = Object.entries(checked).reduce(
+    (init: number[], [key, value]) => {
+      if (value === true) {
+        init.push(parseInt(key));
+      }
+      return init;
+    },
+    []
+  );
 
+  let toggleAccordion = (
+    event: MouseEvent<HTMLDivElement>,
+    data: AccordionTitleProps
+  ) => {
+    const { index } = data;
+    if (typeof index === "undefined") {
+      return setActiveIndex(-1);
+    } else if (typeof index === "string") {
+      return setActiveIndex(-1);
+    }
+
+    const newIndex: number = activeIndex === index ? -1 : index;
+    setActiveIndex(newIndex);
+  };
 
   const diaryentry: DiaryEntry = {
     entry_date: new Date(),
-    skill_ids: checkSkills
+    skill_ids: checkSkills,
   };
 
-  let handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  let submitForm = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    submitDiaryEntry('/diary_entries', diaryentry);
-  }
+    submitDiaryEntry("/diary_entries", diaryentry);
+  };
 
   return (
     <div className="App">
-      <h1 className="ui header"><img src={logo} className="ui circular image" />DBT Skills </h1>
-      <SkillsGroup category={'mindfulness'} categorized_skills={categorized_skills} update_checked={updateChecked} checkedSkills={checked} />
-      <SkillsGroup category={'emotion_regulation'} categorized_skills={categorized_skills} update_checked={updateChecked} checkedSkills={checked} />
-      <SkillsGroup category={'distress_tolerance'} categorized_skills={categorized_skills} update_checked={updateChecked} checkedSkills={checked} />
-      <button className="ui primary button" type="submit" onClick={handleClick}>Submit</button>
-    </div >
+      <h1 className="ui header">
+        <img src={logo} className="ui circular image" />
+        DBT Skills{" "}
+      </h1>
+      <Accordion>
+        <SkillsGroup
+          category={"mindfulness"}
+          categorized_skills={categorized_skills}
+          category_index={0}
+          update_checked={updateChecked}
+          checkedSkills={checked}
+          active_index={activeIndex}
+          handle_click={toggleAccordion}
+        />
+        <SkillsGroup
+          category={"emotion_regulation"}
+          categorized_skills={categorized_skills}
+          category_index={1}
+          update_checked={updateChecked}
+          checkedSkills={checked}
+          active_index={activeIndex}
+          handle_click={toggleAccordion}
+        />
+        <SkillsGroup
+          category={"distress_tolerance"}
+          categorized_skills={categorized_skills}
+          category_index={2}
+          update_checked={updateChecked}
+          checkedSkills={checked}
+          active_index={activeIndex}
+          handle_click={toggleAccordion}
+        />
+      </Accordion>
+      <button className="ui primary button" type="submit" onClick={submitForm}>
+        Submit
+      </button>
+    </div>
   );
 }
 
--- src/components/DisplaySkill.tsx
+++ src/components/DisplaySkill.tsx
@@ -1,70 +1,103 @@
-import { ChangeEvent } from 'react';
-import { Skill } from '../FetchAPI';
+import { ChangeEvent, MouseEvent } from "react";
+import { Accordion, AccordionTitleProps, Icon } from "semantic-ui-react";
+import { Skill } from "../FetchAPI";
 
 interface ToggleSkillProps {
-    skill: Skill,
-    onChecked: (id: number) => void,
-    checked: boolean
+  skill: Skill;
+  onChecked: (id: number) => void;
+  checked: boolean;
 }
-
 
 interface SkillsGroupProps {
-    categorized_skills: {
-        [key: string]: Skill[];
-    },
-    category: string,
-    update_checked: (id: number) => void,
-    checkedSkills: {
-        [key: number]: boolean
-    }
+  categorized_skills: {
+    [key: string]: Skill[];
+  };
+  category: string;
+  update_checked: (id: number) => void;
+  checkedSkills: {
+    [key: number]: boolean;
+  };
+  category_index: number;
+  active_index: number;
+  handle_click: (
+    e: MouseEvent<HTMLDivElement>,
+    data: AccordionTitleProps
+  ) => void;
 }
 
-export function ToggleSkill(props: ToggleSkillProps) {
-    let { skill, onChecked, checked } = props;
-    let onChange = (_event: ChangeEvent<HTMLInputElement>) => {
-        onChecked(skill.id);
-    }
-    
-    return (
-        <div>
-            <div className='ui toggle checkbox'>
-                <h3 className='ui header' style={{ paddingTop: 2 }}>
-                    <input type='checkbox' id={skill.name} name={skill.name} onChange={onChange} checked={!!checked} />
-                    <label>
-                        {skill.name}
-                    </label>
-                </h3>
-            </div>
-        </div >
-    );
-}
+function CapitalizeSkill(word:string): string {
+    return word.split('_').map(str => str[0].toUpperCase() + str.substring(1)).join(' ');
+  };
 
+function ToggleSkill(props: ToggleSkillProps) {
+  let { skill, onChecked, checked } = props;
+  let onChange = (_event: ChangeEvent<HTMLInputElement>) => {
+    onChecked(skill.id);
+  };
+  let skill_name = CapitalizeSkill(skill.name);
+
+  return (
+    <div>
+      <div className="ui toggle checkbox">
+        <p style={{ paddingTop: 2 }}>
+          <input
+            type="checkbox"
+            id={skill.name}
+            name={skill.name}
+            onChange={onChange}
+            checked={!!checked}
+          />
+          <label>{skill_name}</label>
+        </p>
+      </div>
+    </div>
+  );
+}
 
 export function SkillsGroup(props: SkillsGroupProps) {
-    let { category, categorized_skills, update_checked, checkedSkills } = props;
-    let skills_group;
-    if (!categorized_skills[category]) {
-        skills_group = (
-            <div className="ui placeholder">
-                <div className="line"></div>
-                <div className="line"></div>
-            </div>)
-    } else {
-        skills_group = (
-            categorized_skills[category].map((skill: Skill) => {
-                return <ToggleSkill skill={skill} onChecked={update_checked} checked={checkedSkills[skill.id]} key={skill.id} />
-            }))
-    }
-    return (
-        <div>
-            <div className="ui section divider"></div>
-            <div>
-                <h2>{category}</h2>
-                {skills_group}
-            </div>
-        </div>
+  let {
+    category,
+    category_index,
+    categorized_skills,
+    update_checked,
+    checkedSkills,
+    active_index,
+    handle_click,
+  } = props;
+  let skills_group;
+  if (!categorized_skills[category]) {
+    skills_group = (
+      <div className="ui placeholder">
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
     );
-};
-
-
-
+  } else {
+    skills_group = categorized_skills[category].map((skill: Skill) => {
+      return (
+        <ToggleSkill
+          skill={skill}
+          onChecked={update_checked}
+          checked={checkedSkills[skill.id]}
+          key={skill.id}
+        />
+      );
+    });
+  };
+  let CategoryName = CapitalizeSkill(category);
+  return (
+    <>
+      <Accordion.Title
+        active={active_index === category_index}
+        index={category_index}
+        onClick={handle_click}
+      >
+        <Icon name="dropdown" />
+        {CategoryName}
+      </Accordion.Title>
+      <Accordion.Content active={active_index === category_index}>
+        {skills_group}
+      </Accordion.Content>
+    </>
+  );
+}
